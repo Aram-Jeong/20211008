6ba04cfcdb317ec501d250ca0426f56549ff7ac7
82109 <82109@211.38.127.81>
2021-10-08 17:56:30 +0900

    oracle연동 회원관리시스템

---
+++ 20211008/src/main/java/co/micol/prj/MainApp.java
@@ -0,0 +1,13 @@
+package co.micol.prj;
+
+import co.micol.prj.exe.MainMenu;
+
+public class MainApp {
+	public static void main(String[] args) {
+		MainMenu mainMenu = new MainMenu();
+		mainMenu.run();
+	
+		}
+
+	}
+
